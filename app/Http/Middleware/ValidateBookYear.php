d53a8b29f5cb44fdb0e4970645dedadd98304464
Alya Hamidah Izzatul Laili <alyaizza@MacBook-Pro-Alya.local>
2026-03-13 10:01:07 +0700

    first commit

---
+++ app/Http/Middleware/ValidateBookYear.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateBookYear
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $year = $request->year;
+
+        if($year){
+
+            if($year < 1900 || $year > date('Y')){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Invalid book year'
+                ], 400);
+            }
+
+        }
+
+        return $next($request);
+
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+// oute::get('/filterByQuery', [BookController::class, 'filterBooksByQuery']); 
+Route::get(
+    '/filterByParam/{author}/{year}',
+    [BookController::class, 'filterBooksByParam'])->middleware('validate.year');
+Route::get('/filterByQuery', [BookController::class, 'filterBooksByQuery']); 
+
+
+Route::prefix('books')->group(function(){
+
+    Route::get('/',[BookController::class,'getAll']);
+    Route::get('/{id}',[BookController::class,'getOne']);
+    Route::post('/',[BookController::class,'create']);
+    Route::put('/{id}',[BookController::class,'update']);
+    Route::delete('/{id}',[BookController::class,'delete']);
+
+});
