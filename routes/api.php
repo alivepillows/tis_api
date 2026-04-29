94300989e448bf799426ca5d0179e03c0eb7629c
Alya Hamidah Izzatul Laili <alyaizza@MacBook-Pro-Alya.local>
2026-04-29 19:12:27 +0700

    modul 8

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,24 @@
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\TagController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicPostController;
+
+
+Route::prefix('books')->group(function () {
+
+    Route::get('/', [BookController::class, 'getAll'])->name('books.all');
+
+    Route::get('/{id}', [BookController::class, 'getOne']);
+
+    Route::post('/', [BookController::class, 'create']);
+
+    Route::put('/{id}', [BookController::class, 'update']);
+
+    Route::delete('/{id}', [BookController::class, 'delete']);
+});
+
 
 Route::prefix('posts')->group(function () {
     Route::post('/', [PostController::class, 'createPost']);
@@ -19,3 +36,4 @@ Route::prefix('comments')->group(function () {
 Route::prefix('tags')->group(function () {
     Route::post('/', [TagController::class, 'createTag']);
 });
+Route::get('/public-posts', [PublicPostController::class, 'getAllPosts']);
